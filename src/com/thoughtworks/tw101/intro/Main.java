f2da207803efc5b873e11274a460602483159b27
Roberto Sarrionandia <r@sarrionandia.com>
2015-08-30 10:20:44 +0100

    Print vertical line

--- src/com/thoughtworks/tw101/intro/Main.java
+++ src/com/thoughtworks/tw101/intro/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
         printAsterisk();
         printHorizontalLine(8);
+        printVerticalLine(5);
 
     }
 
@@ -18,9 +19,21 @@ public class Main {
      * @param length The length (in chars) of the line
      */
     private static void printHorizontalLine(int length) {
-        for (int i=0; i<length; i++){
+        for (int i=0; i<length; i++) {
             System.out.print('*');
         }
         System.out.println();
     }
+
+    /**
+     * Print a vertical line of asterisks
+     * @param height The height (in console lines) of the line
+     */
+    private static void printVerticalLine(int height) {
+        for (int i=0; i<height; i++) {
+            System.out.println("*");
+        }
+    }
+
+
 }
